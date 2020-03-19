1b8c2b783fca6fbbe9c2fbee7b7eb59c22efc436
Lupa Stance <genzokun@gmail.com>
2020-03-19 03:28:51 +0100

    Adding logo and some features

--- synthTube/synthTube.bat
+++ synthTube/synthTube.bat
@@ -18,12 +18,15 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`%main% -e %url%`) DO (
 
 ECHO // Getting video description
 %main% -x --write-description %url%
-for /f "delims=" %%a in ('dir /b *.description') do set "filename=%%a"
+
+FOR /F "delims=" %%a IN ('dir /b *.description') DO (
+  SET "filename=%%a"
+)
 
 findstr "[0-9]:[0-9] "  "%filename%" > songs.txt
 REM (FOR /F %%A In (songs.txt) Do @SET "_=%%A"&CALL ECHO ^%%_:~-5^%%) > times.txt
 
-DEL "%filename%"
+REM DEL "%filename%"
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`type songs.txt ^| find /C /V ""`) DO (
   SET nsongs=%%F
@@ -33,12 +36,22 @@ ECHO.
 ECHO SONGS: %nsongs%
 ECHO.
 
+MKDIR "%title%"
+CALL DEL "%title%-%url%.description"
+CALL DIR /B "%title%-%url%.*" > ofile.txt
+
+FOR /F "delims=" %%a IN ('type ofile.txt') DO (
+  SET ofile=%%a
+)
+
 SET i=1
 SET x=2
 
 setlocal EnableDelayedExpansion
 
 FOR /F %%a in (songs.txt) do (
+  echo %%a
+  echo ..........................................
   SET elem[!i!]=%%a
   SET /A i+=1
 )
@@ -51,17 +64,20 @@ FOR /L %%b in (1,1,%nsongs%) do (
   ECHO STARTING Track %%b at !ini!
 
   IF %%b LSS %nsongs% (    
-    MKDIR "%title%"
+    
     CALL ECHO ENDING Track %%b at !end!
-    CALL ffmpeg -i "%title%-%url%.m4a" -ss !ini! -to !end! !output!
+    CALL ffmpeg -i "%ofile%" -ss !ini! -to !end! !output!
     
   ) ELSE (
     CALL ECHO Until the end
-    CALL ffmpeg -i "%title%-%url%.m4a" -ss !ini! !output!
+    CALL ffmpeg -i "%ofile%" -ss !ini! !output!
   )
   
   SET /A x+=1
+  ECHO ----------------------
 )
+
+
 
 endlocal
 
