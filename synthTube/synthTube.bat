ebdee2e7bbeb1c1a30d214076b6d0cb926a9b1ba
Lupa Stance <genzokun@gmail.com>
2020-03-21 04:11:19 +0100

    Full performance & deleting cache files ~ v.0.2

--- synthTube/synthTube.bat
+++ synthTube/synthTube.bat
@@ -7,6 +7,14 @@ CLS
 SET url=%1%
 SET main=youtube-dl.exe
 
+IF [%2%]==[] (
+  SET format=mp3
+  ECHO No output format specified, setting it to %format%
+) ELSE (
+  SET format=%2%
+  ECHO Output format selected: %format%
+)
+
 CD bin
 
 ECHO // Getting title
@@ -16,8 +24,10 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`%main% -e %url%`) DO (
   SET title=%%F
 )
 
-ECHO // Getting video description
-%main% -x --write-description %url%
+ECHO // Getting video description ^& thumbnail
+%main% -x --write-description --write-thumbnail %url%
+REN "%title%-%url%.jpg" "cover.jpg"
+
 
 FOR /F "delims=" %%a IN ('dir /b *.description') DO (
   SET "filename=%%a"
@@ -25,8 +35,6 @@ FOR /F "delims=" %%a IN ('dir /b *.description') DO (
 
 findstr "[0-9]:[0-9] "  "%filename%" > songs.txt
 REM (FOR /F %%A In (songs.txt) Do @SET "_=%%A"&CALL ECHO ^%%_:~-5^%%) > times.txt
-
-REM DEL "%filename%"
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`type songs.txt ^| find /C /V ""`) DO (
   SET nsongs=%%F
@@ -50,8 +58,6 @@ SET x=2
 setlocal EnableDelayedExpansion
 
 FOR /F %%a in (songs.txt) do (
-  echo %%a
-  echo ..........................................
   SET elem[!i!]=%%a
   SET /A i+=1
 )
@@ -59,26 +65,32 @@ FOR /F %%a in (songs.txt) do (
 FOR /L %%b in (1,1,%nsongs%) do (
   SET ini=!elem[%%b]!
   SET end=%%elem[!x!]%%
-  SET output="%title%\%title%_%%b.mp3"
+  SET output="%title%\%title%_%%b.%format%"
 
-  ECHO STARTING Track %%b at !ini!
+  ECHO ------------------------------
+  ECHO STARTING Track %%b/%nsongs% at !ini!
 
   IF %%b LSS %nsongs% (    
-    
-    CALL ECHO ENDING Track %%b at !end!
+    CALL ECHO ENDING Track %%b/%nsongs% at !end!
+    ECHO ------------------------------
+    REM CALL ffmpeg -i "%ofile%" -i cover.jpg -map 0 -map 1 -ss !ini! -to !end! !output!
     CALL ffmpeg -i "%ofile%" -ss !ini! -to !end! !output!
     
   ) ELSE (
     CALL ECHO Until the end
+    ECHO ------------------------------
     CALL ffmpeg -i "%ofile%" -ss !ini! !output!
+    
+    :: Borrar el archivo bajado
+    CALL DEL "%ofile%"
   )
   
   SET /A x+=1
-  ECHO ----------------------
 )
 
-
-
 endlocal
+
+MOVE "cover.jpg" "%title%" > NUL
+DEL *.txt
 
 CD..
