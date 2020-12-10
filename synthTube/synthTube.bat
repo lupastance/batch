4c0fb603b0e38207198fd64dec623c1dffbe7832
lupastance <genzokun@gmail.com>
2020-12-10 23:58:13 +0100

    Support new youtube-d version and embed cover to each mp3 file

--- synthTube/synthTube.bat
+++ synthTube/synthTube.bat
@@ -26,7 +26,18 @@ FOR /f "tokens=1 delims=." %%a IN ("%filename%") do (
   SET title=%%a
 )
 
-REN "%title%.jpg" "cover.jpg"
+IF EXIST "%title%.webp" (
+  SET cover=%title%.webp
+  CALL ffmpeg -i "%cover%" "cover.jpg"
+
+) ELSE IF EXIST "%title%.png" (
+  SET cover=%title%.png
+  CALL ffmpeg -i "%cover%" "cover.jpg"
+
+) ELSE (
+  SET cover=%title%.jpg
+  CALL ffmpeg -i "%cover%" "cover.jpg"
+)
 
 :: Filtering list song's list
 findstr "[0-9]:[0-9] "  "%filename%" > songs.txt
@@ -43,6 +54,7 @@ ECHO # ALBUM: %title%
 ECHO # UPLOADER: %uploader%
 
 MKDIR "%title%"
+
 IF EXIST "%title%.opus" (
   CALL DIR /B "%title%.opus" > ofile.txt
 ) ELSE (
@@ -67,38 +79,42 @@ FOR /L %%b in (1,1,%nsongs%) do (
   SET ini=!elem[%%b]!
   SET end=%%elem[!x!]%%
 
-  SET output="%title%\%title%_%%b.%format%"
+  SET output=%title%\%title%_%%b.%format%
   SET metadata=-metadata album="%title%" -metadata album_artist="Various Artists" -metadata artist="%uploader%" -metadata comment="%encoder%" -metadata track="%%b/%nsongs%" -metadata author_url="https://www.youtube.com/watch?v=%url%"
 
   ECHO.
   ECHO CONVERTING Track %%b/%nsongs%
 
+  IF %%b EQU 1 (
+    MKDIR "..\%title%"
+  )
+
   IF %%b LSS %nsongs% (    
     CALL ECHO from !ini! to !end!
-    CALL ffmpeg -hide_banner -loglevel panic -i "%ofile%" -ss !ini! -to !end! !metadata! -c:a libmp3lame -b:a 256k -id3v2_version 3 -write_id3v1 1 !output!
+    CALL ffmpeg -hide_banner -loglevel panic -i "%ofile%" -ss !ini! -to !end! !metadata! -c:a libmp3lame -b:a 256k -id3v2_version 3 -write_id3v1 1 "!output!"
 
     ECHO.
     ECHO --------^| Track %%b converted ^|--------
     
   ) ELSE (
     CALL ECHO from !ini! until the end
-    CALL ffmpeg -hide_banner -loglevel panic -i "%ofile%" -ss !ini! !metadata! -c:a libmp3lame -b:a 256k -id3v2_version 3 -write_id3v1 1 !output!
+    CALL ffmpeg -hide_banner -loglevel panic -i "%ofile%" -ss !ini! !metadata! -c:a libmp3lame -b:a 256k -id3v2_version 3 -write_id3v1 1 "!output!"
     
     ECHO.
     ECHO --------^| Track %%b converted ^|--------
-    
-    :: Delte downloaded audio file
-    CALL DEL "%ofile%"
   )
+
+  CALL ffmpeg -loglevel panic -i "!output!" -i cover.jpg -map 0 -map 1 -c copy -c:v:1 jpg -disposition:v:1 attached_pic "..\!output!"
   
   SET /A x+=1
 )
 
 endlocal
 
-MOVE "cover.jpg" "%title%" > NUL
+DEL "%ofile%"
+DEL cover.jpg
 DEL *.txt
 DEL "%title%.*"
-MOVE "%title%" ..\ > NUL
+RMDIR /Q /S "%title%"
 
 CD..
